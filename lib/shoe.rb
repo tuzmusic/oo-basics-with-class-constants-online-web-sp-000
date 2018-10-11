2c992a3505b2eaa185488a034483e4a54d8792dd
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 17:17:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRAND = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
